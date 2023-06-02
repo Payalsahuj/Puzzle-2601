f7e146d036c7c5a8b87c05a12560bb48064eb59b
Payalsahuj <115584065+Payalsahuj@users.noreply.github.com>
2023-06-03 01:09:17 +0530

    update

---
+++ Basic-II/alanandfrequency.js
@@ -0,0 +1,25 @@
+Screenshot (1323).png
+
+function alanandfrequency(N,str){
+    let obj={};
+    let str1="abcdefghijklmnopqrstuvwxyz";
+    for(let y=0;y<=str1.length-1;y++){
+    for(let x=0;x<=N-1;x++){
+        if(str1[y]==str[x]){
+        if(obj[str[x]]==undefined){
+            obj[str[x]]=1;
+        }
+        else{
+            obj[str[x]]++;
+        }
+        }
+    }
+        
+    }
+    for(let key in obj){
+        console.log(key+"-"+obj[key])
+    }
+    
+}
+
+alanandfrequency(4,aman)
